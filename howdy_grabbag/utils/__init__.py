f2e8aafa7a8abc6580a14ee809045865d0eaab61
Tanim Islam <tanim.islam@gmail.com>
2023-01-24 05:21:41 +0100

    updating howdy_grabbag process mp4 to mkv method.

--- howdy_grabbag/utils/__init__.py
+++ howdy_grabbag/utils/__init__.py
@@ -1,5 +1,5 @@
 import os, sys, time, subprocess, glob, json, titlecase, re
-from distutils.spawn import find_executable
+from shutil import which
 
 def create_epdicts_from_jsonfile( jsonfile ):
     assert( os.path.exists( jsonfile ) )
@@ -24,27 +24,26 @@ def rename_files_in_directory( epdicts, series_name, seasno = 1 ):
 
 def process_mp4_srt_eps_to_mkv(
     epdicts, series_name, seasno = 1, srtglob = '*.srt',
-    ffmpeg_exec = find_executable( 'ffmpeg' ),
-    mkvmerge_exec = find_executable( 'mkvmerge' ),
-    starteps = 1 ):
+    ffmpeg_exec = which( 'ffmpeg' ),
+    mkvmerge_exec = which( 'mkvmerge' ) ):
+    #
+    assert( ffmpeg_exec is not None )
+    assert( mkvmerge_exec is not None )
     mp4files = sorted(glob.glob( '*.mp4'))
     srtfiles = sorted(glob.glob( srtglob ) )
     assert( len(mp4files) == len(srtfiles))
-    epdicts_max = set(map(lambda idx: idx + starteps, range(len(mp4files))))
-    assert( len( epdicts_max - set( epdicts[ seasno ] ) ) == 0 )
-    #ffmpeg_exec   = find_executable( 'ffmpeg' )
-    #mkvmerge_exec = find_executable( 'mkvmerge' )
-    assert( ffmpeg_exec is not None )
-    assert( mkvmerge_exec is not None )
+    #epdicts_max = set(map(lambda idx: idx + starteps, range(len(mp4files))))
+    #assert( len( epdicts_max - set( epdicts[ seasno ] ) ) == 0 )
     assert( all(map(os.path.exists, ( ffmpeg_exec, mkvmerge_exec ) ) ) )
     #
     time00 = time.perf_counter( )
     for idx, tup in enumerate(zip( mp4files, srtfiles ) ):
         time0 = time.perf_counter( )
-        epno = idx + starteps
+        epno = sorted( epdicts[ seasno ] )[ idx ]
         mp4file, srtfile = tup
         newfile = '%s - s%02de%02d - %s.mkv' % (
-            series_name, seasno, epno, epdicts[seasno][epno].replace( '/', ', ' ) )
+            series_name, seasno, epno,
+            epdicts[seasno][epno].replace( '/', ', ' ) )
         stdout_val = subprocess.check_output([
             ffmpeg_exec, '-y', '-i', mp4file, '-codec', 'copy', 'file:%s' % newfile ],
                                              stderr = subprocess.PIPE )
@@ -60,7 +59,7 @@ def process_mp4_srt_eps_to_mkv(
         os.remove( mp4file )
         os.remove( srtfile )
         print( 'processed file %02d / %02d in %0.3f seconds.' % (
-            idx + starteps, max( epdicts_max ), time.perf_counter( ) - time0 ) )
+            idx + 1, len( mp4files ), time.perf_counter( ) - time0 ) )
     #
     print( 'processed all %02d files in %0.3f seconds.' % (
-        max( epdicts_max ), time.perf_counter( ) - time00 ) ) 
+        len( mp4files ), time.perf_counter( ) - time00 ) ) 
