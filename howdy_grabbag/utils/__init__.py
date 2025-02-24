d9f945f4a262bd1446851cf96ccf8bbc47ae6df2
Tanim Islam <tanim.islam@gmail.com>
2025-02-24 06:53:13 +0100

    added in a dirname argument to specify directory into which to rename or process TV files.

--- howdy_grabbag/utils/__init__.py
+++ howdy_grabbag/utils/__init__.py
@@ -1,4 +1,5 @@
 import os, sys, time, subprocess, glob, json, titlecase, re
+from itertools import chain
 from shutil import which
 
 def find_ffmpeg_exec( ):
@@ -21,16 +22,17 @@ def create_epdicts_from_jsonfile( jsonfile ):
                                for idx in epdicts_sub[seasno] } for seasno in epdicts_sub }
     return epdicts
 
-def rename_files_in_directory( epdicts, series_name, seasno = 1 ):
+def rename_files_in_directory( epdicts, series_name, seasno = 1, dirname = os.getcwd( ) ):
     time0 = time.perf_counter( )
     filedict = { idx + 1:filename for (idx, filename) in enumerate(
-        sorted(glob.glob('*.mp4') + glob.glob("*.avi") + glob.glob("*.mkv") + glob.glob('*.wmv') + glob.glob( '*.flv') + glob.glob( '*.webm' ))) }
+        sorted(chain.from_iterable(map(lambda suffix: glob.glob( os.path.join( dirname, "*.%s" % suffix ) ),
+                                       ( "mp4", "avi", "mkv", "wmv", "flv", "webm"))))) }
     assert( seasno in epdicts )
     assert( len( set( epdicts[ seasno ] ) - set( filedict ) ) == 0 )
     for epno in sorted( filedict ):
         suffix = re.sub( ".*\.", "", filedict[epno]).strip()
-        newfile = "%s - s%02de%02d - %s.%s" % (
-            series_name, seasno, epno, epdicts[seasno][epno], suffix )
+        newfile = os.path.join( dirname, "%s - s%02de%02d - %s.%s" % (
+            series_name, seasno, epno, epdicts[seasno][epno], suffix ) )
         os.rename( filedict[ epno ], newfile)
     print( 'processed %02d files in %0.3f seconds.' % (
         len( filedict ), time.perf_counter( ) - time0 ) )
@@ -38,12 +40,13 @@ def rename_files_in_directory( epdicts, series_name, seasno = 1 ):
 def process_mp4_srt_eps_to_mkv(
     epdicts, series_name, seasno = 1, srtglob = '*.srt',
     ffmpeg_exec = which( 'ffmpeg' ),
-    mkvmerge_exec = which( 'mkvmerge' ) ):
+    mkvmerge_exec = which( 'mkvmerge' ),
+    dirname = os.getcwd( ) ):
     #
     assert( ffmpeg_exec is not None )
     assert( mkvmerge_exec is not None )
-    mp4files = sorted(glob.glob( '*.mp4'))
-    srtfiles = sorted(glob.glob( srtglob ) )
+    mp4files = sorted(glob.glob( os.path.join( dirname, '*.mp4' ) ) )
+    srtfiles = sorted(glob.glob( os.path.join( dirname, srtglob ) ) )
     assert( len(mp4files) == len(srtfiles))
     #epdicts_max = set(map(lambda idx: idx + starteps, range(len(mp4files))))
     #assert( len( epdicts_max - set( epdicts[ seasno ] ) ) == 0 )
@@ -54,20 +57,20 @@ def process_mp4_srt_eps_to_mkv(
         time0 = time.perf_counter( )
         epno = sorted( epdicts[ seasno ] )[ idx ]
         mp4file, srtfile = tup
-        newfile = '%s - s%02de%02d - %s.mkv' % (
+        newfile = os.path.join( dirname, '%s - s%02de%02d - %s.mkv' % (
             series_name, seasno, epno,
-            epdicts[seasno][epno].replace( '/', ', ' ) )
+            epdicts[seasno][epno].replace( '/', ', ' ) ) )
         stdout_val = subprocess.check_output([
             ffmpeg_exec, '-y', '-i', mp4file, '-codec', 'copy', 'file:%s' % newfile ],
                                              stderr = subprocess.PIPE )
         try:
             stdout_val = subprocess.check_output([
-                mkvmerge_exec, '-o', 'default.mkv', newfile,
+                mkvmerge_exec, '-o', os.path.join( dirname, 'default.mkv' ), newfile,
                 '--language', '0:eng', '--track-name', '0:English', srtfile ],
                                                  stderr = subprocess.PIPE )
         except Exception as e:
             pass
-        os.rename( 'default.mkv', newfile )
+        os.rename( os.path.join( dirname, 'default.mkv' ), newfile )
         os.chmod( newfile, 0o644 )
         os.remove( mp4file )
         os.remove( srtfile )
