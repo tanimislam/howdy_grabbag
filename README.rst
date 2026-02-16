55272d905e2d601f9a7dcf9ea57117425076f4c1
Tanim Islam <tanim.islam@gmail.com>
2026-02-16 12:26:28 -0800

    aborted attempt (emacs TRAMP hell) to improve the README, develop Sphinx documentation.

--- README.rst
+++ README.rst
@@ -3,17 +3,25 @@
 ===============================================================================
 This repository consists of a bunch of random utility scripts that use, or are associated with, Howdy_. Howdy_ is an SDK used to manage movies, television shows, and music located on a Plex_ server. Its relatively comprehensive documentation (missing documentation on two of its GUIs) lives in the `Howdy documentation page <https://tanimislam.github.io/howdy>`_.
 
-This module depends explicitly on howdy_ and mutagen_. This module will not install and run without ffmpeg_, ffprobe_, and mkvmerge_. This module will install without HandBrakeCLI_, but with reduced functionality -- no transcoding in convert_mp4movie_to_mkv_.
+This module depends explicitly on howdy_ and mutagen_. This module will not run without nice_, ffmpeg_, ffprobe_, mkvmerge_, mkvpropedit_, and HandBrakeCLI_.
 
-Installing this software is unusual but straightforward. Just run this command, using ``pip3`` (pip_ for Python 3),
+Installing this software is unusual but straightforward. Just run this command using pip_ for Python 3,
 
 .. code-block:: console
 
-   pip3 install --user --upgrade git+https://github.com/tanimislam/howdy_grabbag#egg=howdy_grabbag
+   python3 -m pip install --user --upgrade git+https://github.com/tanimislam/howdy_grabbag#egg=howdy_grabbag
 
-And then these executables (`convert_mp4movie_to_mkv <convert_mp4movie_to_mkv_label_>`_ and `fix_permissions <fix_permissions_label_>`_ for now), will live in ``~/.local/bin``.
+You can also install them by downloading the repository from Github directly, then installing,
 
-The documentation starts with a description of each command line tool in this README, and grows from there. As this repository evolves, the documentation *may* become more organized. Right now, it is what it is.
+.. code-block:: console
+
+   python3 -m pip install --user -e .
+
+Currently, there are *nineteen* executables as part of ``howdy_grabbag``. The Sphinx documentation now documents each of the command line tools in this repository. We invite users to look in the Sphinx documentation for this project to understand how all these tools work.
+   
+.. And then these executables (`convert_mp4movie_to_mkv <convert_mp4movie_to_mkv_label_>`_ and `fix_permissions <fix_permissions_label_>`_ for now), will live in ``~/.local/bin``.
+
+.. The documentation starts with a description of each command line tool in this README, and grows from there. As this repository evolves, the documentation *may* become more organized. Right now, it is what it is.
 
 .. _convert_mp4movie_to_mkv_label:
 
--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -10,9 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os, sys, datetime, re
+from functools import reduce
+from sphinx.util import logging
+_mainDir = reduce(lambda x,y: os.path.dirname( x ),
+                  range(2), os.path.abspath('.'))
+sys.path.insert( 0, _mainDir )
+
+logger = logging.getLogger( __name__ )
+logger.debug( "mainDir = %s" % _mainDir)
 
 
 # -- Project information -----------------------------------------------------
@@ -20,6 +26,8 @@
 project = 'howdy_grabbag'
 copyright = '2022, Tanim Islam'
 author = 'Tanim Islam'
+
+tls_verify = True
 
 # The full version, including alpha/beta/rc tags
 release = '0.5'
@@ -31,7 +39,40 @@ release = '0.5'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.mathjax',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinxcontrib.youtube',
+    'sphinx_issues',
+    'sphinxarg.ext',
 ]
+
+#
+## following instructions here (https://github.com/svenevs/exhale/tree/master/docs/_intersphinx) to fix beautifulsoup doc.
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'requests': ( 'https://requests.readthedocs.io/en/latest', None),
+    'beautifulsoup' : ( 'https://www.crummy.com/software/BeautifulSoup/bs4/doc', "_intersphinx/bs4_objects.inv"),
+    'geoip2' : ( 'https://geoip2.readthedocs.io/en/latest', None),
+    'gmusicapi' : ( 'https://unofficial-google-music-api.readthedocs.io/en/latest', None ),
+    'cinemagoer' : ( 'https://cinemagoer.readthedocs.io/en/latest', None),
+    'pyqt5' : ( 'https://www.riverbankcomputing.com/static/Docs/PyQt5', "_intersphinx/pyqt5_objects.inv" ),
+    'requests_oauthlib' : ( 'https://requests-oauthlib.readthedocs.io/en/latest', None ),
+    'oauth2client' : ( 'https://oauth2client.readthedocs.io/en/latest', None ),
+    'google-auth' : ( 'https://google-auth.readthedocs.io/en/latest', None ),
+    'deluge' : ( 'https://deluge.readthedocs.io/en/latest', None ),
+    'transmission_rpc' : ( 'https://transmission-rpc.readthedocs.io/en/stable', None ),
+    'plexapi' : ( 'https://python-plexapi.readthedocs.io/en/latest', None ),
+    'sqlalchemy' : ( 'https://docs.sqlalchemy.org', None ),
+    'subliminal' : ( 'https://subliminal.readthedocs.io/en/latest/', None ),
+    'musicbrainzngs' : ( 'https://python-musicbrainzngs.readthedocs.io/en/latest', None ),
+    'pillow' : ( 'https://pillow.readthedocs.io/en/stable', None ),
+    'pandas' : ( 'http://pandas.pydata.org/pandas-docs/stable', None ),
+}
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -41,15 +82,31 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+#
+## numfig stuff
+numfig = True
+
+
+#
+## GitHub repo
+issues_github_path = "tanimislam/howdy"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_sidebars = {
+   '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+   'using/windows': ['windowssidebar.html', 'searchbox.html'],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
